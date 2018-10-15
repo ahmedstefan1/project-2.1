9660c145d462400338538aed38ad9ac981675b96
ahmedstefan1 <a.s.el.kharbotly@st.hanze.nl>
2018-10-15 14:03:19 +0200

    default size of window

--- python programma/window.py
+++ python programma/window.py
@@ -1,9 +1,10 @@
 from tkinter import *
 
+
 class window:
-    def __init__(self):
+    def __init__(self, width=500, height=250):
         root = Tk()
-        frame = Frame(root)
+        frame = Frame(root, width=width, height=height)
         frame.grid()
 
 
