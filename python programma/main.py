42df1ab6da2b4130d936df229a12189a812e5bb5
ahmedstefan1 <a.s.el.kharbotly@st.hanze.nl>
2018-10-15 13:47:40 +0200

    main en window

---
+++ python programma/main.py
@@ -0,0 +1,8 @@
+from window import *
+
+def main():
+    w = window()
+
+
+if __name__ == '__main__':
+    main()
---
+++ python programma/window.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class window:
+    def __init__(self):
+        root = Tk()
+        frame = Frame(root)
+        frame.pack()
+
+
+        root.mainloop()
+
