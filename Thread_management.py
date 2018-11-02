599e7224a817bb75326e98c082e5c462f687ebb0
ahmedstefan1 <a.s.el.kharbotly@st.hanze.nl>
2018-11-02 12:39:44 +0100

    packet decoding ingevoerd

--- Thread_management.py
+++ Thread_management.py
@@ -5,3 +5,8 @@ def background(func):
     global thread
     thread = Thread(target=func)
     thread.start()
+
+def backgroundarg(func, arg):
+    global thread
+    thread = Thread(target=func, args=arg)
+    thread.start()
--- linker.py
+++ linker.py
@@ -1,5 +1,7 @@
 import serial
 import serial.tools.list_ports as com_ports
+import binascii
+from Thread_management import *
 
 connection = serial.Serial()
 
@@ -23,7 +25,7 @@ def serial_connection(com):
     global connection
     connection = serial.Serial(com, 19200)
     print(connection)
-    readlines()
+    getpacket()
 
 # sluit de seriële connectie
 def close_connection():
@@ -34,13 +36,40 @@ def close_connection():
         print("no connection is open")
 
 # leest de wat de arduino verstuurt
-def readlines():
+def getpacket():
     global connection
     while True:
-        x = connection.read()
-        print(x)
+        x = connection.readline()
+        backgroundarg(protocol_understanding, (binascii.hexlify(x),))
+
 
 # hoort characters te versturen
-def send(data=hex(237)):
+def sendpacket(data=hex(237)):
     global connection
     connection.write(data)
+
+
+# understands the protocol and checks for mistakes
+def protocol_understanding(data):
+    # slices the data to what is needed
+    sliced_data = data[0:4]
+
+    # puts the data in different variables
+    sensor = sliced_data[0:1]
+    waarde = sliced_data[1:3]
+    check = sliced_data[3:4]
+    # xors the front part of the data with the back part, er is wat omrekenen nodig
+    # Xor werkt alleen met Int en niet met hexadecimalen
+    U1 = int(waarde[0:1], 16) ^ int(waarde[1:2], 16)
+    # xors de data van de sensor met uitkomst1 (U1)
+    U2 = U1 ^ int(sensor, 16)
+    # kijkt of die waarden overeen komen ( als ze niet overeen komen is er waarschijnlijk ergens een fout ontstaan
+    if U2 == int(check, 16):
+        # welke sensor komt het uit
+        if sensor == b'8':
+            # print de waarde van de sensor naar de console
+            print("temperatuur:" + str(int(waarde, 16)) + u'\u00B0' + "C")
+        # elif sensor =
+        # elif sensor =
+        # elif sensor =
+
