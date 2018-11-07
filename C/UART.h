945824b55e58aa1ecead8d9360af7ec92de3aa14
ahmedstefan1 <a.s.el.kharbotly@st.hanze.nl>
2018-11-07 13:43:39 +0100

    werkende licht en temperatuur sensor

--- C/UART.h
+++ C/UART.h
@@ -1,4 +1,4 @@
-
+// headers van alle functies in UART.C
 void uart_init(void);
 void transmit(uint8_t data);
 char recieve(void);
--- C/main.c
+++ C/main.c
@@ -1,51 +1,71 @@
-#include<avr/io.h>
-#include<stdlib.h>
-#include<avr/sfr_defs.h>
-#define F_CPU 16E6
-#include<util/delay.h>
+#include <avr/io.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <avr/sfr_defs.h>
 #include "UART.h"
-#include "AVR_TTC_scheduler.h"
+
+#define F_CPU 16E6
+#include <util/delay.h>
+
+#define UBBRVAL 51
+
+#define HIGH 0x1
+#define LOW  0x0
+
+int ADCsingleREAD(uint8_t ADCport)
+{
+	int ADCvalue;
+
+	ADMUX = ADCport;        // use #1 ADC
+	ADMUX |= (1 << REFS0);   // use AVcc as the reference
+	ADMUX &= ~(1 << ADLAR);  // clear for 10 bit resolution
+
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+	ADCSRA |= (1 << ADEN);    // Enable the ADC
+	ADCSRA |= (1 << ADSC);    // Start the ADC conversion
+
+	while(ADCSRA & (1 << ADSC)); // waits for the ADC to finish
+
+	ADCvalue = ADCL;
+	ADCvalue = (ADCH << 8) + ADCvalue; // ADCH is read so ADC can be updated again
+
+	return ADCvalue;
+}
+
+
+uint8_t getadc()
+{
+	double ADCvalue = 0;
+	int degrees = 0;
+	while (1) {
+		// reads temperature
+		ADCvalue = ADCsingleREAD(1);
+		// rekent om naar graden C
+		degrees = ((ADCvalue / 1024 * 5)-0.5)*100;
+		// zet het om naar een int
+		degrees = (int) degrees;
+
+		transmit(degrees);
+		// reads light sensor light sensor doesnt need conversion
+		transmit(ADCsingleREAD(0));
+		_delay_ms(1000);
+
+	}
+}
 
 
 
-
-int main(void){
+int main()
+{
+	// initialiseert uart
 	uart_init();
-	//SCH_Init_T1();
-	//unsigned char Task_x;
-	//unsigned char Task_y;
-	//Task_x = SCH_Add_Task(task,0,10);
-	//Task_y = SCH_Add_Task(task,0,10);
-	//SCH_Start();
-	//while(1){
-	//	SCH_Dispatch_Tasks();
-	//}
-	_delay_ms(1000);
 	while(1){
-		// voorbeeld code voor seriele verbinding
 		_delay_ms(1000);
-		transmit(0x83);
-		transmit(0x29);
-		transmit(0x0A);
-		
-		transmit(0x10);
-		transmit(0x10);
-		transmit(0x0A);
-		_delay_ms(1000);
-		
-		transmit(0x10);
-		transmit(0x23);
-		transmit(0x0A);
-		
-		_delay_ms(1000);
-		transmit(0x24);
-		transmit(0x06);
-		transmit(0x0A);
-		
-		_delay_ms(1000);
-		transmit(0x42);
-		transmit(0x06);
-		transmit(0x0A);
+		// haalt de waarde van de analoge sensoren op
+		getadc();
+
 	}
 	return(0);
 }
+
